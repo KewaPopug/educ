f3cda7ac452840b8b8763fb999966b5effee1ee4
Alexander <almittt@mail.ru>
2022-11-20 01:17:06 +0600

    refactor: https://trello.com/c/NXoNh7EJ/5-%D0%BF%D0%B5%D1%80%D0%B5%D0%B4%D0%B5%D0%BB%D0%B0%D1%82%D1%8C-%D0%BC%D0%B0%D1%80%D1%88%D1%80%D1%83%D1%82%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8E-%D0%B4%D0%BB%D1%8F-%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8-%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8F
    Добавлен список TODO

--- routes/web.php
+++ routes/web.php
@@ -1,49 +1,67 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
+	use Illuminate\Support\Facades\Auth;
+	use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+	/*
+	|--------------------------------------------------------------------------
+	| Web Routes
+	|--------------------------------------------------------------------------
+	|
+	| Here is where you can register web routes for your application. These
+	| routes are loaded by the RouteServiceProvider within a group which
+	| contains the "web" middleware group. Now create something great!
+	|
+	*/
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+	//TODO убрать закомментированные строки. Они здесь больше ни к чему
+	//Route::get('/', function () {
+	//    return view('welcome');
+	//});
 
-Route::view('/home', 'home');
+	//TODO Route::view('/home', 'home') нигде не используется в системе. Зачем он тогда нужен? Удали его
+	Route::view('/home', 'home');
 
-Route::name('user.')->group(function (){
-    Route::view('/private', 'private')->middleware('auth')->name('private');
+	//TODO некорректное название группы маршрутов(name('user.')). Поскольку данная группа отвечает за авторизацию пользователей, то лигичнее назвать её auth
+	Route::name('user.')->group(function () {
 
-    Route::get('/login', function (){
-        if(Auth::check()) {
-            return redirect(route('user.private'));
-        }
-        return view('login');
-    })->name('login');
+		//FIXME:
+		// private - это плохое название для данного маршрута. Так как подразумевается, что данный маршрут будет вести на главную страницу сайта, то его лучше назвать main, либо home
+		// Данный маршрут необходимо убрать из данной группы маршрутов т.к. он не относится к авторизации напрямую
+		Route::view('/private', 'private')->middleware('auth')->name('private');
 
-    Route::post('/login', [\App\Http\Controllers\LoginController::class, 'login']);
+		Route::get('/login', function () {
+			//FIXME:
+			// Никогда не пиши логику в маршрутах. Маршруты лучше всего использовать для перенаправления в контроллеры за редким исключением
+			// Перенеси блок if (Auth::check()) в метод login() контроллера /var/www/educ/app/Http/Controllers/LoginController.php
+			if (Auth::check()) {
+				return redirect(route('user.private'));
+			}
+			//TODO перенаправление view также перенести в метод login() контроллера /var/www/educ/app/Http/Controllers/LoginController.php
+			return view('login');
+		})->name('login');
 
-    Route::get('/logout', function (){
-        Auth::logout();
-        return  redirect('/');
-    })->name('logout');
+		// TODO классы лучше всего подключать через команду use т.к. \App\Http\Controllers\LoginController является очень длинной строкой и код становится плохо читаемым
+		Route::post('/login', [\App\Http\Controllers\LoginController::class, 'login']);
 
-    Route::get('/registration', function (){
-        if(Auth::check()) {
-            return redirect(route('user.private'));
-        }
-        return view('registration');
-    })->name('registration');
+		//FIXME:
+		// В этом маршруте также присутствует ненужная логика.
+		// Создай метод logout() в контроллере \App\Http\Controllers\LoginController и перенеси логику Auth::logout() туда
+		Route::get('/logout', function () {
+			Auth::logout();
 
+			return redirect('/');
+		})->name('logout');
 
-    Route::post('/registration', [\App\Http\Controllers\RegisterController::class, 'save']);
-});
+		// TODO поскольку в нашей системе не предусмотрена регистрация, то данный маршрут нецелесообразен. Удали его
+		Route::get('/registration', function () {
+			if (Auth::check()) {
+				return redirect(route('user.private'));
+			}
+
+			return view('registration');
+		})->name('registration');
+
+		//TODO также удали этот маршрут
+		Route::post('/registration', [\App\Http\Controllers\RegisterController::class, 'save']);
+	});
