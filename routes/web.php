789a9e9719e78d81e2eab380196cb4e6feefc97e
kewa <rombik2004@mail.ru>
2022-11-24 22:15:48 +0600

    fix: update routes
    update routes and controllers

--- routes/web.php
+++ routes/web.php
@@ -15,34 +15,16 @@ use Illuminate\Support\Facades\Auth;
 	|
 	*/
 
-	//TODO убрать закомментированные строки. Они здесь больше ни к чему+
+    Route::view('/', 'welcome');
 
 
-	//TODO Route::view('/home', 'home') нигде не используется в системе. Зачем он тогда нужен? Удали его+
+    Route::name('auth.')->group(function () {
 
-	//TODO некорректное название группы маршрутов(name('user.')). Поскольку данная группа отвечает за авторизацию пользователей, то лигичнее назвать её auth+
-	Route::name('auth.')->group(function () {
+        Route::view('/main', 'main')->middleware('auth')->name('main');
 
-		//FIXME:
-		// private - это плохое название для данного маршрута. Так как подразумевается, что данный маршрут будет вести на главную страницу сайта, то его лучше назвать main, либо home
-		// Данный маршрут необходимо убрать из данной группы маршрутов т.к. он не относится к авторизации напрямую+
-		Route::view('/main', 'main')->middleware('auth')->name('main');
+        Route::view('/login', 'login')->name('login');
 
-		Route::get('/login', function () {
-			//FIXME:
-			// Никогда не пиши логику в маршрутах. Маршруты лучше всего использовать для перенаправления в контроллеры за редким исключением
-			// Перенеси блок if (Auth::check()) в метод login() контроллера /var/www/educ/app/Http/Controllers/LoginController.php+
+        Route::post('/login', [LoginController::class, 'login']);
 
-
-			//TODO перенаправление view также перенести в метод login() контроллера /var/www/educ/app/Http/Controllers/LoginController.php
-			return view('login');
-		})->name('login');
-
-		// TODO классы лучше всего подключать через команду use т.к. \App\Http\Controllers\LoginController является очень длинной строкой и код становится плохо читаемым+
-		Route::post('/login', [LoginController::class, 'login']);
-
-		//FIXME:
-		// В этом маршруте также присутствует ненужная логика.
-		// Создай метод logout() в контроллере \App\Http\Controllers\LoginController и перенеси логику Auth::logout() туда+
-        Route::get('/logout', [LoginController::class, 'logout']);
+		Route::get('/logout', [LoginController::class, 'logout']);
 	});
